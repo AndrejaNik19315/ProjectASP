735436b7560b00a2f3909cb216a1c43732878dfe
LonelyLion193 <35309992+LonelyLion193@users.noreply.github.com>
2019-05-15 20:58:42 +0200

    Added Application layer, small changes on Domain layer.

--- Application/Dto/UserDto.cs
+++ Application/Dto/UserDto.cs
@@ -1,16 +1,16 @@
-﻿using System;
+﻿using Domain;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace Domain
+namespace Application.Dto
 {
-    public class User : BaseEntity
+    public class UserDto : BaseEntityDto
     {
         public string Firstname { get; set; }
         public string Lastname { get; set; }
         public string Username { get; set; }
         public string Email { get; set; }
         public bool IsActive { get; set; }
-        public IEnumerable<Character> Characters { get; set; }
     }
 }

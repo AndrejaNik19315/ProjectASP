4d30fecd5b31bb4c0f66c4b9ff034cb82f7edfd3
AndrejaNik19315 <35309992+AndrejaNik19315@users.noreply.github.com>
2019-05-15 21:24:47 +0200

    Revert "Revert "Added Application layer, small changes on Domain layer.""
    This reverts commit b0f40e32d7283b72256495459c25458489aa1282.

---
+++ Application/Dto/BaseEntityDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Dto
+{
+    public partial class BaseEntityDto
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ Application/Dto/CharacterDto.cs
@@ -0,0 +1,19 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Dto
+{
+    public class CharacterDto : BaseEntityDto
+    {
+        public string Name { get; set; }
+        public int Level { get; set; }
+        public decimal? Funds { get; set; }
+        public int GameClassId { get; set; }
+        public int GenderId { get; set; }
+        public int RaceId { get; set; }
+        public int? InventoryId { get; set; }
+        public Inventory Inventory { get; set; }
+    }
+}

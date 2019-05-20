b12381fab399481c0c4b9622a68e2540ec625a61
AndrejaNik19315 <35309992+AndrejaNik19315@users.noreply.github.com>
2019-05-20 21:17:44 +0200

    Added ClientApp(Api)

---
+++ Api/Queries/UserSearchQuery.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Queries
+{
+    public class UserSearchQuery
+    {
+        public string Username { get; set; }
+    }
+}
